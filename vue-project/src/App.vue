9044084d209bb667c5189f43696a915b95af47b4
UdeeshaRukshan <IT21807480@my.sliit.lk>
2026-08-26 19:01:33 +0530

    New main page added to page folder

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,11 +1,23 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+import MainPage from './pages/MainPage.vue'
+
+const showMainPage = ref(false)
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <MainPage v-if="showMainPage" />
+
+  <template v-else>
+    <h1>You did it!</h1>
+    <p>
+      Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+      documentation
+    </p>
+    <button class="btn btn-primary" type="button" @click="showMainPage = true">
+      <i class="bi bi-plus-circle"></i>
+    </button>
+  </template>
 </template>
 
 <style scoped></style>
---
+++ vue-project/src/pages/MainPage.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="container mt-5">
+    <h1>My Learning Tracker</h1>
+
+    <button class="btn btn-primary">
+      <i class="bi bi-plus-circle"></i>
+    </button>
+  </div>
+</template>
